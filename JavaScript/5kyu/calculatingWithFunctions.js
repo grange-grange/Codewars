8bf5e820d1bf399bbacf02fa71ab834f02fc080e
Aliaksandr Fisiuk <123926431+grange-grange@users.noreply.github.com>
2023-04-06 14:01:24 +0300

    .

--- JavaScript/5kyu/calculatingWithFunctions.js
+++ JavaScript/5kyu/calculatingWithFunctions.js
@@ -12,6 +12,9 @@ Each calculation consist of exactly one operation and two numbers
 The most outer function represents the left operand, the most inner function represents the right operand
 Division should be integer division. For example, this should return 2, not 2.666666...:
 eight(dividedBy(three()));*/
+
+//#1
+
 function calc(n, o) {return Array.isArray(o) ? Math.floor(n * o[0]) : o ? n + o : n}
 
 function zero(o)  {return calc(0, o)}
@@ -29,3 +32,20 @@ function plus(n) {return n}
 function minus(n) {return -n}
 function times(n) {return [n]}
 function dividedBy(n) {return [1/n]}
+
+//#2
+function zero(o)  {return o ? o(0) : 0}
+function one(o)   {return o ? o(1) : 1}
+function two(o)   {return o ? o(2) : 2}
+function three(o) {return o ? o(3) : 3}
+function four(o)  {return o ? o(4) : 4}
+function five(o)  {return o ? o(5) : 5}
+function six(o)   {return o ? o(6) : 6}
+function seven(o) {return o ? o(7) : 7}
+function eight(o) {return o ? o(8) : 8}
+function nine(o)  {return o ? o(9) : 9}
+
+function plus(b) {return (a) => a + b}
+function minus(b) {return (a) => a - b}
+function times(b) {return (a) => a * b}
+function dividedBy(b) {return (a) => Math.floor(a / b)}
