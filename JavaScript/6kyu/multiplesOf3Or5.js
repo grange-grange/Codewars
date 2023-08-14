dd3c7d6663771f43d259b5b089262951f858d663
Aliaksandr Fisiuk <123926431+grange-grange@users.noreply.github.com>
2023-08-14 11:55:46 +0300

    Update multiplesOf3Or5.js

--- JavaScript/6kyu/multiplesOf3Or5.js
+++ JavaScript/6kyu/multiplesOf3Or5.js
@@ -3,7 +3,7 @@
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
 
 Note: If the number is a multiple of both 3 and 5, only count it once.*/
- function solution(number){
+function solution(number){
   if (number < 3) return 0
   let solution = 0
   for (let i = number - 1; i >= 3; i--) {
